806d8f7db549ecb0679b074cf8028bc72a596c90
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2013-12-23 12:15:38 -0800

    OK, data is actually loading correctly now.

--- colourlovers/main.cpp
+++ colourlovers/main.cpp
@@ -2,10 +2,10 @@
 #include "GLUT/glut.h"
 #include "Eigen/Core"
 
+#include <iostream>
 #include <string>
 #include <vector>
 #include <fstream>
-#include <sstream>
 #include <tr1/unordered_set>
 
 using namespace std;
@@ -25,26 +25,28 @@ void loadSamples(const string& patternIDPath, const unordered_set<double>& which
 	ifstream infile(filename.c_str());
 	char line[1024];
 	bool doneHeaders = false;
+	const char* delims = " ,|";
 	while (!infile.eof())
 	{
 		infile.getline(line, 1023);
-		if (doneHeaders)
+		string linestr = string(line);
+		if (doneHeaders && !linestr.empty())
 		{
-			stringstream ss(line);
-			string dummy;	// For commas, pipes, etc.
-			string id;
-			double temp;
-			double score;
-			ss >> id >> dummy >> temp >> dummy >> score >> dummy;
-			if (whichTemps.count(temp))
+			string id = string(strtok(line, delims));
+			string temp_str = string(strtok(NULL, delims));
+			double temp = atof(temp_str.c_str());
+			double score = atof(strtok(NULL, delims));
+			if (whichTemps.count(temp) > 0)
 			{
 				// Assuming 5 colors
 				VectorXd colors(15);
 				for (int i = 0; i < 5; i++)
 				{
-					ss >> colors[3*i] >> colors[3*i+1] >> colors[3*i+2] >> dummy;
+					colors[3*i] = atof(strtok(NULL, delims));
+					colors[3*i+1] = atof(strtok(NULL, delims));
+					colors[3*i+2] = atof(strtok(NULL, delims));
 				}
-				string imageFilename = patternIDPath + string("/") + id + string(".png");
+				string imageFilename = patternIDPath + string("/") + temp_str + string("/") + id + string(".png");
 				FIBITMAP* image = FreeImage_Load(FIF_PNG, imageFilename.c_str());
 				SampleRecord srec;
 				srec.score = score;
@@ -65,11 +67,13 @@ int main(int argc, char** argv)
 	FreeImage_Initialise();
 
 	// Load data
+	cout << "Loading data..." << flush;
 	char* patternIDPath = argv[1];
 	unordered_set<double> whichTemps;
 	whichTemps.insert(0.05);
 	vector<SampleRecord> sampleRecords;
 	loadSamples(patternIDPath, whichTemps, sampleRecords);
+	cout << "DONE (" << sampleRecords.size() << " samples loaded)." << endl;
 
 	// Find and project islands
 
